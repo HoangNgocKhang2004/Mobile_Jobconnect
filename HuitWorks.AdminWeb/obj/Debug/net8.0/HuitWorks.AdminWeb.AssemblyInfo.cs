7f5e660d347f350e184f792a486e2f9e62e942cd
HoangNgocKhang2004 <hoangngockhang.huit@gmail.com>
2025-04-12 22:09:34 +0700

    commit

--- HuitWorks.AdminWeb/obj/Debug/net8.0/HuitWorks.AdminWeb.AssemblyInfo.cs
+++ HuitWorks.AdminWeb/obj/Debug/net8.0/HuitWorks.AdminWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HuitWorks.AdminWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+590b1f8eaa38dec20d9573259406c70b0b3ef3f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50c5896f3334e078d41841a60cefd6771738a6a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("HuitWorks.AdminWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HuitWorks.AdminWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
