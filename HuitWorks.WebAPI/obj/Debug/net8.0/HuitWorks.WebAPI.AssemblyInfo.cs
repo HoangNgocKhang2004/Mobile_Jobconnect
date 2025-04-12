37e8f82c1e711abc2336d20c4415c199e06c7390
HoangNgocKhang2004 <hoangngockhang.huit@gmail.com>
2025-04-12 19:55:41 +0700

    commit

--- HuitWorks.WebAPI/obj/Debug/net8.0/HuitWorks.WebAPI.AssemblyInfo.cs
+++ HuitWorks.WebAPI/obj/Debug/net8.0/HuitWorks.WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HuitWorks.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6970b61631302841bb39d933f7d71d91a3db5ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("HuitWorks.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HuitWorks.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
